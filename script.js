5e0d7b77363af696586257de833f5768591f540a
Corey Taylor <kcmocane@gmail.com>
2022-02-18 19:41:18 -0600

    Assignment 5 attempt 1

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+//const { formSubmission } = require("./scriptHelper");
+
+//const { myFetch } = require("./scriptHelper");
+
+//const { myFetch } = require("./scriptHelper");
+
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    let list = document.getElementById("faultyItems");
+    list.style.visibility = 'hidden';
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    // Below this comment call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination.
+        let planet = pickPlanet (listedPlanets);
+        addDestinationInfo (document,planet.name,planet.diameter,planet.star,planet.distance,planet.moons,planet.image);
+    })
+   let form = document.querySelector ("form");
+   form.addEventListener("submit", function(event){
+       event.preventDefault ();
+       let pilot = document.querySelector ("input[name=pilotName]").value;
+       let copilot = document.querySelector ("input[name=copilotName]").value;
+       let fuelLevel = document.querySelector ("input[name=fuelLevel]").value;
+       let cargoMass = document.querySelector ("input[name=cargoMass]").value;
+       let list = document.getElementById ("faultyItems");
+       formSubmission (document, list, pilot, copilot, fuelLevel, cargoMass)
+    });
 });
